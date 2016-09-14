71a66a92bd885b5e34415b27170f2d1b093f11ac
Rony Nasr <nasr.rony@gmail.com>
2016-09-14 16:47:15 -0700

    added devise user authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
+  get 'page/home'
+
+  get 'page/about'
+
+  get 'page/contact'
+
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root :to => 'users#index'
+  root :to => 'page#home'
 
   resources :users do
     resources :bikes, :except => [:index]
