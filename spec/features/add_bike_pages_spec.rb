0d841c9ba5454beab3a379297d97aa84faa21488
Rony Nasr <nasr.rony@gmail.com>
2016-10-03 11:04:53 -0700

    added working ajax integration testing

--- spec/features/add_bike_pages_spec.rb
+++ spec/features/add_bike_pages_spec.rb
@@ -27,9 +27,19 @@ describe "the add bike process" do
       visit user_path(user)
       click_link "New Bike"
       expect(page).to have_content "Make"
-      fill_in "make", :with => "BMW"
+      fill_in "Make", :with => "BMW"
       click_on 'Create Bike'
       expect(page).to have_content 'BMW'
     end
 
+    it "adds a bike from user page using AJAX", js: true do
+      user =FactoryGirl.create(:user)
+      login_as(user, :scope => :user)
+      visit user_path(user)
+      click_link "New Bike"
+      expect(page).to have_content "Make"
+      fill_in "Make", :with => ""
+      click_on 'Create Bike'
+      expect(page).to have_content 'New Bike'
+    end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,5 +4,6 @@ describe User do
   it {should validate_presence_of :name}
   it {should have_many :reviews}
   it {should have_many :bikes}
+  it {should have_many :places}
 
 end
